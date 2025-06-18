4399f63d6551bc693de4ca03e9ad6fbfd00b3840
Daniyal <daniyal06383@gmail.com>
2025-06-19 01:34:59 +0500

    commited in master

--- new.c
+++ new.c
@@ -4,6 +4,9 @@ int main(){
     for(int i =0; i<5;i++){
         printf("Hello world!!!!!!\n");
     }
+    for(int i =0; i<5;i++){
+        printf("Mein Inki Maut Hun Meine Bil Aakhir Aana Hai!!\n");
+    }
     return 0;
 }
 
